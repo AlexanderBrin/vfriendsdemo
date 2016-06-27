ed4546e0f94fa49c00e6007cae40d3c0dc8dbb4a
Alexander Brin <alexanderbrin@icloud.com>
2016-06-27 17:43:15 +0300

    Initial Commit

---
+++ vkfriends/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  vkfriends
+//
+//  Created by Alexander on 27/06/16.
+//  Copyright © 2016 Alexander Brin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ vkfriendsTests/vkfriendsTests.m
@@ -0,0 +1,39 @@
+//
+//  vkfriendsTests.m
+//  vkfriendsTests
+//
+//  Created by Alexander on 27/06/16.
+//  Copyright © 2016 Alexander Brin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface vkfriendsTests : XCTestCase
+
+@end
+
+@implementation vkfriendsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
